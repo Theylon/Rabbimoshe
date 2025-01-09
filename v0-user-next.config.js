f1f6fb3ab44fc9c5fffce75d815b00fda7afa178
Theeylon <aviveylon2@gmail.com>
2025-01-09 13:21:41 +0200

    Add files via upload

---
+++ v0-user-next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: './',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
+
